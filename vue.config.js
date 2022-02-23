f5ebe8adf7ed526de91bb3885d55d2b95ed64caa
Branden <brandenbao@gmail.com>
2022-02-23 04:21:30 -0800

    removed public path...

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: '/bodenbao.github.io/'
+  transpileDependencies: true
+  //publicPath: '/bodenbao.github.io/'
 })
